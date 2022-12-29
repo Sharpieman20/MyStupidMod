eccc04525f22d3a0c213f4a51a324856065617c2
Sharpieman20 <>
2022-12-29 03:01:47 -0600

    Thank u redlime

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/render/chunk/RebuildTaskMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/chunk/RebuildTaskMixin.java
@@ -1,6 +1,7 @@
 package me.voidxwalker.worldpreview.mixin.client.render.chunk;
 
 import net.minecraft.client.render.chunk.ChunkBuilder;
+import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.objectweb.asm.Opcodes;
@@ -19,26 +20,31 @@ public class RebuildTaskMixin {
 //    @Final
 //    private static Logger LOGGER;
 
-    private void myCodeInner() {
+    public RebuildTaskMixin() {
+
+        
+    }
+
+    private void myCodeInner(ChunkRendererRegion region) {
 
         throw new IllegalStateException();
     }
 
     @Redirect(method = "run(Lnet/minecraft/client/render/chunk/BlockBufferBuilderStorage;)Ljava/util/concurrent/CompletableFuture;", at = @At(value = "FIELD", target = targetString, opcode = Opcodes.PUTFIELD))
-    private void injected(ChunkBuilder.BuiltChunk.RebuildTask rebuildTask, Object x) {
+    private void injected(ChunkBuilder.BuiltChunk.RebuildTask instance, ChunkRendererRegion value) {
 
-        myCodeInner();
+        myCodeInner(value);
     }
 
     @Redirect(method = "render(FFFLnet/minecraft/client/render/chunk/ChunkBuilder$ChunkData;Lnet/minecraft/client/render/chunk/BlockBufferBuilderStorage;)Ljava/util/Set;", at = @At(value = "FIELD", target = targetString, opcode = Opcodes.PUTFIELD))
-    private void injected2(ChunkBuilder.BuiltChunk.RebuildTask rebuildTask, Object x) {
+    private void injected2(ChunkBuilder.BuiltChunk.RebuildTask instance, ChunkRendererRegion value) {
 
-        myCodeInner();
+        myCodeInner(value);
     }
 
     @Redirect(method = "cancel()V", at = @At(value = "FIELD", target = targetString, opcode = Opcodes.PUTFIELD))
-    private void injected3(ChunkBuilder.BuiltChunk.RebuildTask rebuildTask, Object x) {
+    private void injected3(ChunkBuilder.BuiltChunk.RebuildTask instance, ChunkRendererRegion value) {
 
-        myCodeInner();
+        myCodeInner(value);
     }
 }

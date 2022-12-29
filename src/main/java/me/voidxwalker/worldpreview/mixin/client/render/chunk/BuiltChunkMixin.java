5fd912ee0040821ffb80c46ccf22bf0c7d520027
Sharpieman20 <>
2022-12-29 02:21:06 -0600

    Add my stupid code

--- src/main/java/me/voidxwalker/worldpreview/mixin/client/render/chunk/BuiltChunkMixin.java
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/chunk/BuiltChunkMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.screen.LevelLoadingScreen;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.chunk.ChunkBuilder;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
---
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/chunk/RebuildTaskMixin.java
@@ -0,0 +1,44 @@
+package me.voidxwalker.worldpreview.mixin.client.render.chunk;
+
+import net.minecraft.client.render.chunk.ChunkBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ChunkBuilder.BuiltChunk.RebuildTask.class)
+public class RebuildTaskMixin {
+
+    private static final String targetString = "net/minecraft/client/render/chunk/ChunkBuilder$BuiltChunk$RebuildTask.region:Lnet/minecraft/client/render/chunk/ChunkRendererRegion;";
+
+//    @Shadow
+//    @Final
+//    private static Logger LOGGER;
+
+    private void myCodeInner() {
+
+        throw new IllegalStateException();
+    }
+
+    @Redirect(method = "run(Lnet/minecraft/client/render/chunk/BlockBufferBuilderStorage;)Ljava/util/concurrent/CompletableFuture;", at = @At(value = "FIELD", target = targetString, opcode = Opcodes.PUTFIELD))
+    private void injected(ChunkBuilder.BuiltChunk.RebuildTask rebuildTask, Object x) {
+
+        myCodeInner();
+    }
+
+    @Redirect(method = "render(FFFLnet/minecraft/client/render/chunk/ChunkBuilder$ChunkData;Lnet/minecraft/client/render/chunk/BlockBufferBuilderStorage;)Ljava/util/Set;", at = @At(value = "FIELD", target = targetString, opcode = Opcodes.PUTFIELD))
+    private void injected2(ChunkBuilder.BuiltChunk.RebuildTask rebuildTask, Object x) {
+
+        myCodeInner();
+    }
+
+    @Redirect(method = "cancel()V", at = @At(value = "FIELD", target = targetString, opcode = Opcodes.PUTFIELD))
+    private void injected3(ChunkBuilder.BuiltChunk.RebuildTask rebuildTask, Object x) {
+
+        myCodeInner();
+    }
+}
